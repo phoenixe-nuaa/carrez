32d0b8f50eab6e8330d87fba8b9dd600835711a8
phoenixe-nuaa <913438300@qq.com>
2016-02-01 05:37:05 +0800

    Step 7 - UX/UI
    	modified:   app.js
    	new file:   public/css/style.css
    	modified:   public/js/main.js
    	modified:   views/index.ejs

--- app.js
+++ app.js
@@ -17,15 +17,38 @@ app.get('/', function (req, res) {
 app.post('/ajax', function (req, res) {
 	// var url = "http://www.leboncoin.fr/ventes_immobilieres/915700197.htm?ca=12_s"
 	var url = req.body.url;
-	// console.log(req.query);
+
+	// test command
+	// var result = {
+	// 			status: "success",
+	// 			result: {
+	// 				type: "Appartemnet",
+	// 				city: "Levallois-Perret",
+	// 				postal_code: "92300",
+	// 				price_per_m2: 8593.75,
+	// 				good_deal: false,
+	// 				reference_price: {
+	// 					lowest: 5658,
+	// 					average: 7545,
+	// 					highest: 11317
+	// 				}
+	// 			}
+	// 		};
+	// res.json(result);
+	// return;
+
 	leboncoin(url, function (err, data) {
 		meilleursagents(data, function (err, data) {
-			res.json(data);
+			var result = {
+				status: "success",
+				result: data
+			}
+			res.json(result);
 		});
 	});
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(3000, "127.0.0.1", function () {
   var host = server.address().address;
   var port = server.address().port;
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,54 @@
-$('#compute').click(function () {
+$("#compute").click(function () {
+	(function init() {
+		$("#compute").addClass("loading");
+		$("#result").fadeOut();
+		$("#answer").removeClass("green");
+		$("#values").removeClass("green");
+		$("#answer").removeClass("yellow");
+		$("#values").removeClass("yellow");
+	}());
+
 	var post_url = $("#url").val();
 	$.post('./ajax', {
 		url: post_url
 	}, function (data, status) {
-		if(status == "success") {
-			if(data.good_deal) {
-				$("#result").text("Good deal");
+		$("#compute").removeClass("loading");
+		if(status == "success" && data.status == "success") {
+			$("#compute").addClass("green");
+			$("#compute").text("Succeed");
+			setTimeout(function () {
+				$("#compute").removeClass("green");
+				$("#compute").text("Evaluate");
+			}, 3000);
+
+			if(data.result.good_deal) {
+				$("#answer").addClass("green");
+				$("#answer").text("Good deal");
+				$("#values").addClass("green");
 			}
 			else {
-				$("#result").text("Not a good deal");
+				$("#answer").addClass("yellow");
+				$("#answer").text("Not a good deal");
+				$("#values").addClass("yellow");
 			}
+
+			$("#values>tbody>tr:eq(0)>td:last").text(data.result.city+" "+data.result.postal_code);
+			$("#values>tbody>tr:eq(1)>td:last").text(data.result.type);
+			$("#values>tbody>tr:eq(2)>td:last").text(data.result.price_per_m2.toFixed(2)+" €");
+			$("#values>tbody>tr:eq(3)>td:last").text(data.result.reference_price.lowest+" €");
+			$("#values>tbody>tr:eq(4)>td:last").text(data.result.reference_price.average+" €");
+			$("#values>tbody>tr:eq(5)>td:last").text(data.result.reference_price.highest+" €");
+
+			$("#result").fadeIn();
+		}
+		else {
+			// Failed to wrap data
+			$("#compute").addClass("red");
+			$("#compute").text("Failed");
+			setTimeout(function () {
+				$("#compute").removeClass("red");
+				$("#compute").text("Evaluate");
+			}, 3000);
 		}
 	});
 });
